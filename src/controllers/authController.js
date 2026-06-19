aea4222aa47d57dd4798e331fd166f30adcd1339
SudheerKumarDas <samratsudheerdas@gmail.com>
2026-06-19 14:15:41 +0545

    user logged in logic

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,4 +1,5 @@
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 import User from "../models/User.js";
 
@@ -31,7 +32,64 @@ export const register = async (req, res) => {
     });
   } catch (error) {
     console.error("Register Error:", error);
+    return res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
+};
 
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(400).json({
+        message: "All fields are required",
+      });
+    }
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found",
+      });
+    }
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
+    if (!isPasswordMatch) {
+      return res.status(401).json({
+        message: "Invalid credentials",
+      });
+    }
+    const accessToken = jwt.sign(
+      {
+        id: user._id,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "15m",
+      },
+    );
+    const refreshToken = jwt.sign(
+      {
+        id: user._id,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "7d",
+      },
+    );
+    user.refreshToken = refreshToken;
+    await user.save();
+
+    res.status(200).json({
+      message: "User logged in successfully",
+      user: {
+        name: user.name,
+        email: user.email,
+      },
+      accessToken,
+      refreshToken,
+    });
+  } catch (error) {
+    console.error("Login Error:", error);
     return res.status(500).json({
       message: "Internal Server Error",
     });
