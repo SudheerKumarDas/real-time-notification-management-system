eccfbdfa6ff0fbf5d31a32d68d744e42af47cdf8
SudheerKumarDas <samratsudheerdas@gmail.com>
2026-06-23 22:54:11 +0545

    users can follow each other

--- src/models/User.js
+++ src/models/User.js
@@ -1,35 +1,42 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-    username:{
-        type:String,
-        required:true,
-        unique:true
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    email:{
-        type:String,
-        required:true,
-        unique:true
+    email: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    password:{
-        type:String,
-        required:true
+    password: {
+      type: String,
+      required: true,
     },
-    refreshToken:String,
-    following:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:"User",
-        default:[]
-    },
-    followers:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:"User",
-        default:[]
-    }
-}, {
-    timestamps:true
-});
+    refreshToken: String,
+    following: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        default: [],
+      },
+    ],
+    followers: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        default: [],
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  },
+);
 
-const User = mongoose.model("User",userSchema);
+const User = mongoose.model("User", userSchema);
 
 export default User;
